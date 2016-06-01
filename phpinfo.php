a4b21b44b70ac2c9275bc3de51f49aaa51fac21e
wuge123 <452925601@qq.com>
2016-06-02 00:41:38 +0800

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+function env($key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return $default;
+    }
+    return $value;
+}
+echo env('MYSQL_USERNAME');
